05bdcc2728745061fd6a8b04cc091a087daa1f07
Paulo Guilherme <pauloguilherme@usp.br>
2023-09-24 16:11:22 -0300

    Update main.sh and project organization

--- main.sh
+++ main.sh
@@ -2,19 +2,14 @@
 
 check_compilation(){
     if [ $? -eq 0 ]; then
-        echo "Running experiment..."
+        echo "Running simulation..."
     else
         echo "Compilation failed."
         exit 1
     fi
 }
 
-
-if [ -z "$1" ]; then
-    echo "No argument supplied."
-elif [ -z "$2" ]; then
-    echo "Compiling and running src/$1.cpp..."
-    g++ -o $1  src/$1.cpp -lGL -lGLU -lglut
-    check_compilation
-    ./$1
-fi
+echo "Compiling and running src/main.cpp..."
+g++ -o main  src/main.cpp -lGL -lGLU -lglut
+check_compilation
+./main
